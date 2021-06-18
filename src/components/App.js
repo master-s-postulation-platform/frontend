22f62a5f2021a7a9557d20257ed972280cac5938
devcabezas <luiscabezas25@hotmail.com>
2021-06-18 16:05:07 -0500

    Componente landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
 import Footer from './footer/Footer';
+import Landing from './landing/Landing';
 
 class App extends React.Component {
   render() {
     return(
       <>
         <Header />
+        <Landing />
         <Footer />
       </>
     )
---
+++ src/components/landing/Landing.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Lupa from '../icons/Lupa';
+import Bombilla from '../icons/Bombilla';
+import House from '../icons/House';
+import '../../styles/global.scss';
+
+class Landing extends React.Component {
+  render() {
+    return(
+      <>
+        <div className="landing">
+          <p className="landing__title">
+            Para postularte en Talent Jobs <br/>
+            debe tener en cuenta los <br/>
+            siguientes pasos.
+          </p>
+          <div className="landing__container">
+            <button className="landing__btn">
+              <Lupa/>
+              <p>Crea una cuenta</p>
+            </button>
+            <button className="landing__btn1">
+              <Bombilla/>
+              <p>Llena tú perfil</p>
+            </button>
+            <button className="landing__btn2">
+              <House/>
+              <p>Enviar <br/>postulación</p>
+            </button>
+          </div>
+          <button className="landing__submit">
+            INICIAR POSTULACIÓN
+          </button>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Landing;

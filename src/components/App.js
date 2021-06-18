d769bfcd3ddaf6a63c9b3b1bd0e3829666f27f8f
devcabezas <luiscabezas25@hotmail.com>
2021-06-17 21:15:27 -0500

    configuración webpack

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './icons/Logo';
+
+class App extends React.Component {
+  render() {
+    return(
+      <>
+        <Logo />
+        <h1>Talento nuevo</h1>
+      </>
+    )
+  }
+}
+
+export default App;

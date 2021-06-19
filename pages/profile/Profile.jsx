47cb91a239db1da8b9c30c75b349f50aaabd4f4f
Jc <jcpablo10@gmail.com>
2021-06-19 15:12:50 -0500

    profile page

---
+++ pages/profile/Profile.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Header from '../../src/components/header/Header'
+import Footer from '../../src/components/footer/Footer'
+import ButtonPrimary from '../../src/components/buttons/ButtonPrimary'
+
+const Profile = () => {
+    return (
+        <section className="section">
+            <section className="section__background">
+                <section className="section__content">
+                    <section className="section__grid">
+                    <form className="form">
+                        <label className="form__label" htmlFor="apellidos">
+                            <span>Apellidos:</span>
+                            <input className="form__input" type="text" placeholder="Apellidos" name="apellidos"/>
+                        </label>
+                        <label className="form__label" htmlFor="nombre">
+                            <span>Nombre:</span>
+                            <input className="form__input" type="text" placeholder="Nombre" name="nombre"/> 
+                        </label>
+                        <div className="form__column--2">
+                            <label className="form__label" htmlFor="sexo">
+                                    <span>Sexo:</span>
+                                <select name="sexo" className="form__select">
+                                    <option>Selecciona una opción</option>
+                                    <option value="femenino">Femenino</option>
+                                    <option value="masculino">Masculino</option>
+                                </select>
+                            </label>
+
+                            <label className="form__label" htmlFor="fecha_nacimiento">
+                                <span>Fecha de nacimiento:</span>
+                                <input className="form__date" type="date" name="fecha_nacimiento" /> 
+                            </label>
+                        </div>
+                        <div className="form__column--2">
+                            <label className="form__label" htmlFor="estado_civil">
+                                <span>Estado civil:</span>
+                                <select name="estado_civil" className="form__select">
+                                    <option>Selecciona una opción</option>
+                                    <option value="femenino">Femenino</option>
+                                    <option value="masculino">Masculino</option>
+                                </select>
+                            </label>
+                            <label className="form__label" htmlFor="pais_residencia">
+                                <span>País de residencia</span>
+                                <select name="pais_residencia" className="form__select">
+                                    <option disabled>Selecciona una opción</option>
+                                    <option value="femenino">Colombia</option>
+                                    <option value="masculino">México</option>
+                                </select>
+                            </label>
+
+                        </div>
+                        <div className="form__footer">
+                            <button className="secondary__button">Guardar parcial</button>
+                            <button className="primary__button">Guardar y continuar</button>
+                        </div>
+                    </form>
+                    </section>
+                </section>
+            </section>
+        </section>
+    )
+}
+
+export default Profile

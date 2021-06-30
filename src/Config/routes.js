efcc499864e6f04fa2bc96a05cd3ac54818b6832
F2Design <tu-email@gmail.comfredyfrasser@hotmail.com>
2021-06-30 09:24:26 -0500

    Cambios dashboard

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -11,53 +11,59 @@ import Education from "../pages/profile/Education";
 import Idioms from "../pages/profile/Idioms";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
 import Dashboard from "../pages/Dashboard/Dashboard.jsx";
+import VistaAdmin from "../pages/VistaAdmin/VistaAdmin";
 
 const routes = [
-    {
-        path:'/',
-        component: Landing,
-        isPrivate: false
-    },
-    {
-        path:'/login',
-        component: Login,
-        isPrivate: false
-    },
-    {
-        path: '/register',
-        component: Register,
-        isPrivate: false
-    },
-    {
-        path: '/profile',
-        component: Profile,
-        isPrivate: true
-    },
-    {
-        path: '/profesional',
-        component: ProfesionalExperience,
-        isPrivate: true
-    },
-    {
-        path: '/education',
-        component: Education,
-        isPrivate: true
-    },
-    {
-        path: '/idioms',
-        component: Idioms,
-        isPrivate: true
-    },
-    {
-        path: '/dashboard',
-        component: Dashboard,
-        isPrivate: true
-    },
-    {
-        path: '/*',
-        component: PageNotFound,
-        isPrivate: true
-    }
-]
+  {
+    path: "/",
+    component: Landing,
+    isPrivate: false,
+  },
+  {
+    path: "/login",
+    component: Login,
+    isPrivate: false,
+  },
+  {
+    path: "/register",
+    component: Register,
+    isPrivate: false,
+  },
+  {
+    path: "/profile",
+    component: Profile,
+    isPrivate: true,
+  },
+  {
+    path: "/profesional",
+    component: ProfesionalExperience,
+    isPrivate: true,
+  },
+  {
+    path: "/education",
+    component: Education,
+    isPrivate: true,
+  },
+  {
+    path: "/idioms",
+    component: Idioms,
+    isPrivate: true,
+  },
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    isPrivate: true,
+  },
+  {
+    path: "/vistaadmin",
+    component: VistaAdmin,
+    isPrivate: true,
+  },
+  {
+    path: "/*",
+    component: PageNotFound,
+    isPrivate: true,
+  },
+];
 
 export default routes;
---
+++ src/pages/VistaAdmin/VistaAdmin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./vistaAdmin.scss";
+const VistaAdmin = () => {
+  return (
+    <>
+      <h1>Vista administracion</h1>
+    </>
+  );
+};
+
+export default VistaAdmin;

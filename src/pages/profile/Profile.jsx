5f99ec3ad4d263e5d76d2ba96652b416fcfc1454
Jc <jcpablo10@gmail.com>
2021-07-09 13:41:48 -0500

    complete profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useAuthState } from '../../Context/index.js'
 
 const Profile = () => {
@@ -8,7 +8,15 @@ const Profile = () => {
     const [first_name, setName] = useState("");
     const [last_name, setLastName] = useState("");
     const [username, setUsername] = useState("")
-    const [gender, setGender] = useState(1)
+    const [gender, setGender] = useState(0)
+    const [birthday, setBirthday] = useState("")
+    const [country, setCountry] = useState()
+    const [c_status, setCivil] = useState()
+    const [address_line1, setAddress] = useState("")
+    const [home_phone, setHomePhone] = useState("")
+    const [mobile_phone, setMobilePhone] = useState("")
+
+    /* ref */
     
     useEffect(()=> {
         fetch('https://api.hardmakers.com/api/v1/profile/information/',{
@@ -17,17 +25,21 @@ const Profile = () => {
         })
         .then(response => response.json())
         .then( data => {
-            console.log('Test education')
+            
             console.log(data)
 
-            setName(data.data.profile[0].user.first_name)
-            setLastName(data.data.profile[0].user.last_name)
-            setUsername(data.data.profile[0].user.username)
-            setGender(data.data.profile[0].user.gender)
-            // if (data.data.profile[0].user.username === "") {
-                
-            // } else {
-            // }
+            const {first_name, last_name, username } = data.data.profile[0].user;
+
+            setName(first_name)
+            setLastName(last_name)
+            setUsername(username)
+            setGender(data.data.profile[0].gender)
+            setBirthday(data.data.profile[0].birthday)
+            setCountry(data.data.profile[0].Address.country.id)
+            setCivil(data.data.profile[0].civil_status.id)
+            setAddress(data.data.profile[0].Address.address_line1)
+            setHomePhone(data.data.profile[0].home_phone)
+            setMobilePhone(data.data.profile[0].mobile_phone)
 
         })
 
@@ -40,13 +52,13 @@ const Profile = () => {
             'first_name': first_name,
             "username": username,
             "last_name": last_name,
-            "birthday": "2021-07-07",
-            "c_status": 1,
-            "country":1,
+            "birthday": birthday,
+            "c_status": c_status,
+            "country": country,
             "city":"1",
-            "address_line1":"",
-            "home_phone":"",
-            "mobile_phone":"",
+            "address_line1":address_line1,
+            "home_phone":home_phone,
+            "mobile_phone":mobile_phone,
             "gender": gender
         }
 
@@ -65,13 +77,6 @@ const Profile = () => {
             console.log(data)
         })
     }
-
-    const handleGender = (e) => {
-        setGender(e.target.value)
-        console.log(gender)
-    }
-    
-
 
     return (
         <section className="section">
@@ -98,31 +103,31 @@ const Profile = () => {
                             <input className="form__input" type="text" placeholder="Nombre" name="first_name" value={first_name} onChange={ e => (setName(e.target.value))}/> 
                         </label>
                         <div className="form__column--2">
-                            <label className="form__label" htmlFor="sexo">
+                            <label className="form__label" htmlFor="gender">
                                     <span>Sexo:</span>
-                                <select name="gender" className="form__select" onChange={handleGender}>
+                                <select name="gender" className="form__select" value={gender} onChange={ e => (setGender(e.target.value))}>
                                     {/* <option>Selecciona una opción</option> */}
-                                    <option value={2}>Femenino</option>
                                     <option value={1}>Masculino</option>
+                                    <option value={2}>Femenino</option>
                                 </select>
                             </label>
 
                             <label className="form__label" htmlFor="birthday">
                                 <span>Fecha de nacimiento:</span>
-                                <input className="form__date" type="date" name="birthday" pattern="\y{4}-\m{2}-\d{2}"/> 
+                                <input className="form__date" type="date" name="birthday" pattern="\y{4}-\m{2}-\d{2}" onChange={(e)=> {setBirthday(e.target.value)}} value={birthday}/> 
                             </label>
                         </div>
                         <div className="form__column--2">
                             <label className="form__label" htmlFor="c_status">
                                 <span>Estado civil:</span>
-                                <select name="c_status" className="form__select">
+                                <select name="c_status" className="form__select" value={c_status} onChange={e => (setCivil(e.target.value))}>
                                     <option disabled>Selecciona una opción</option>
-                                    <option value="1">Soltero</option>
-                                    <option value="2">Casado</option>
-                                    <option value="3">Divorciado</option>
-                                    <option value="4">Separación en proceso</option>
-                                    <option value="5">Viudo/a</option>
-                                    <option value="6">Concubinato</option>
+                                    <option value={1}>Casado</option>
+                                    <option value={2}>Soltero</option>
+                                    <option value={3}>Divorciado</option>
+                                    <option value={4}>Concubinato</option>
+                                    
+                                    
                                 </select>
                             </label>
                         </div>
@@ -130,27 +135,27 @@ const Profile = () => {
                         <h5>Dirección:</h5>
                         <label className="form__label" htmlFor="countries">
                             <span>País de residencia</span>
-                            <select name="countries" className="form__select">
+                            <select name="countries" className="form__select" onChange={ e => (setCountry(e.target.value))} value={country} >
                                 <option disabled>Selecciona una opción</option>
-                                <option value="1">México</option>
-                                <option value="2">Colombia</option>
-                                <option value="3">Chile</option>
+                                <option value={1}>México</option>
+                                <option value={2}>Colombia</option>
+                                <option value={3}>Chile</option>
                             </select>
                         </label>
                         <label className="form__label" htmlFor="address_line1">
                             <span>Dirección de residencia:</span>
-                            <input className="form__input" type="text" placeholder="Direccion" name="address_line1"/>
+                            <input className="form__input" type="text" placeholder="Direccion" name="address_line1" value={address_line1} onChange={ e => (setAddress(e.target.value))} />
                         </label>
 
                         <h5>Contacto:</h5>
                         <div className="form__column--2">
                             <label className="form__label" htmlFor="mobile_phone">
                                 <span>Teléfono fijo:</span>
-                                <input className="form__input" type="number" name="mobile_phone" /> 
+                                <input className="form__input" type="number" name="mobile_phone" value={mobile_phone} onChange={e => (setMobilePhone(e.target.value))}/> 
                             </label>
                             <label className="form__label" htmlFor="home_phone">
                                 <span>Número movil:</span>
-                                <input className="form__input" type="number" name="home_phone" /> 
+                                <input className="form__input" type="number" name="home_phone" value={home_phone} onChange={e => (setHomePhone(e.target.value))}/> 
                             </label>
                         </div>
 
